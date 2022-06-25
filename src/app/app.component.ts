3989596bb9350ac25b936264bbb36bd1eef1bf13
maxrporto <maxrporto@gmail.com>
2022-06-25 09:24:19 -0300

    adicionando o 2° commit do projeto curso-alura - serviços

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,13 +9,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bytebank';
 
-  transferencia: any;
+ // transferencias: any[] = [];
   //destino: number;
   //valor: number;
 
+  constructor(private service: TransferenciaService){
+
+  }
+
   transferir($event){
-    console.log($event);
-    this.transferencia = $event;
+    this.service.adicionar($event);
+
+
+    /**
+     * operador spread operator (...) , desconstrói o objeto $event
+     * >> Ou seja, se estão chegando duas propriedades, que são valor e destino, ao invés de pegar o objeto e passar para dentro de transferencia
+     * estou pegando apenas as propriedades internas dele e passando para dentro do objeto transferencia.
+     */
+    //const transferencia = {...$event, data: new Date()}
+    //this.transferencias.push(transferencia);
+   //this.transferencias.push($event);
     //this.destino = $event.destino;
     //this.valor =$event.valor;
   }
