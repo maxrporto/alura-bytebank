dd152dc3c9a6a55540d865e0087e182e01828f4a
maxrporto <maxrporto@gmail.com>
2022-06-25 06:33:28 -0300

    adicionando o 1° commit do projeto curso-alura

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bytebank';
+
+  transferencia: any;
+  //destino: number;
+  //valor: number;
+
+  transferir($event){
+    console.log($event);
+    this.transferencia = $event;
+    //this.destino = $event.destino;
+    //this.valor =$event.valor;
+  }
+}
---
+++ src/app/nova-tranferencia/nova-transferencia.component.ts
@@ -0,0 +1,28 @@
+import { Component, Output } from '@angular/core';
+import { EventEmitter }  from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+  valor: number;
+  destino: number;
+
+  tranferir() {
+    console.log('solicitado nova tranferência');
+   // console.log('valor :' , this.valor);
+   // console.log('destino :' , this.destino);
+
+    this.aoTransferir.emit({valor: this.valor, destino: this.destino});
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
